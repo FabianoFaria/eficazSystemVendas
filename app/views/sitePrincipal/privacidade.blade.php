6f39dec631c6efac81b820af1df23633ed8bbdff
Fabiano Hatori <sistemaeficaz@sistema.eficazsystem.com.br>
2017-11-30 17:56:52 -0200

    Definição dos termos de uso e de privacidade

--- app/views/sitePrincipal/privacidade.blade.php
+++ app/views/sitePrincipal/privacidade.blade.php
@@ -2,6 +2,196 @@
 
 @section('content')
 
+	<section class="features" id="">
 
+		<div class="container">
+
+			<h3 class="text-center">POLÍTICA DE PRIVACIDADE <img src="https://parcerias.eficazsystem.com.br/img/logo-eficaz-system.png" width="200" height="75" alt="Eficaz System" border="0"></h3>
+
+			<p class="text-left text-muted">
+				<!-- Estes termos e condições gerais de uso aplicam-se aos serviços prestados pela pessoa jurídica, devidamente registrada sob o CNPJ n. 08.488.358/0001-17 , com sede em: -->
+
+				Nós, da Eficaz System (Eficaz System Eireli - Epp), estamos comprometidos em resguardar sua privacidade. O intuito deste documento é esclarecer quais informações são coletadas dos usuários de nossos sites <a href="https://parcerias.eficazsystem.com.br" target="blanc">https://parcerias.eficazsystem.com.br/</a> – e respectivos serviços – e de que forma esses dados são manipulados e utilizados.
+			</p>
+
+			<p class="text-left text-muted">
+				Alertamos que se você não concorda com o conteúdo desta política, não é recomendável baixar nossos materiais nem utilizar quaisquer de nossos serviços.
+			</p>
+
+			<!-- <p class="text-left text-muted">
+				Rua Norberto de Brito, 1131
+			</p>
+			<p class="text-left text-muted">
+				São José dos Pinhais,
+			</p>
+			<p class="text-left text-muted">
+				Paraná
+			</p>
+
+			<p class="text-left text-muted">
+				por meio do site <b>Parcerias Eficaz System</b>, com o seguinte endereço: <a href="https://parcerias.eficazsystem.com.br" target="blanc">https://parcerias.eficazsystem.com.br/</a>
+			</p> -->
+
+
+			<!-- 
+
+				Nós, da Resultados Digitais (RD GESTÃO E SISTEMAS SA), estamos comprometidos em resguardar sua privacidade. O intuito deste documento é esclarecer quais informações são coletadas dos usuários de nossos sites https://resultadosdigitais.com.br, http://rdstation.com.br, http://rdsummit.com.br e http://rdontheroad.com.br – e respectivos serviços – e de que forma esses dados são manipulados e utilizados.
+
+				Alertamos que se você não concorda com o conteúdo desta política, não é recomendável baixar nossos materiais nem utilizar quaisquer de nossos serviços.
+
+			-->
+
+			<ol>
+
+				<li>Sobre a coleta de dados</li>
+				<br>
+
+				<p>
+					Em nosso site, as informações são coletadas das seguintes formas:
+				</p>
+
+				<p>
+					Informações fornecidas por você – Coletamos informações de identificação pessoal – como nome, telefone, email, empresa em que trabalha e cargo – via preenchimento dos formulários para acesso ao nossos programas de parcerias de vendas. Eventualmente, a solicitação algumas informações podem ser feitas por meio de contato direto da Eficaz System com os usuários via email ou telefone.
+				</p>
+
+				<p>
+					Informações de navegação no site – Quando você visita nosso site, é inserido um ‘cookie’ no seu navegador por meio do software Google Analytics, para identificar quantas vezes você retorna ao nosso endereço. São coletadas, anonimamente, informações, como endereço IP, localização geográfica, fonte de referência, tipo de navegador, duração da visita e páginas visitadas.
+				</p>
+
+				<p>
+					Histórico de contato – Aqui na Eficaz System armazenamos informações a respeito de todos os contatos já realizados com nossos usuários, como conteúdos baixados a partir de nossas páginas e interações via email.
+				</p>
+
+				<li>Sobre o uso de suas Informações Pessoais</li>
+				<br>
+
+				<p>
+					O presente termo permite que a Eficaz System use suas informações pessoais para diferentes finalidades.
+				</p>
+
+				<p>
+					O email é utilizado para a operação de envio do material ou informação por você requisitada no preenchimento do formulário. Também pode ser usado para envio de Newsletters, sempre relacionadas ao tema de Parceria de vendas da Eficaz System.
+				</p>
+
+				<p>
+					Por fim, o email será utilizado ainda para comunicar o lançamento de novos materiais gratuitos, não gratuitos ou de novos produtos da Eficaz System e parceiros. No entanto, o usuário pode cancelar a parceria a qualquer momento.
+				</p>
+
+				<p>
+					Os dados de acesso, participação ou atividades poderão ser divulgados como pesquisas e estatísticas, não sendo reveladas abertamente nenhuma informação pessoal, a menos que autorizada explicitamente.
+				</p>
+
+				<p>
+					Funcionários da Eficaz System poderão eventualmente entrar em contato via email ou telefone para fazer pesquisas ou apresentar produtos e serviços.
+				</p>
+
+				<p>	
+					Ao solicitar a participação no programa de Parceria de vendas da Eficaz System, o usuário recebe um termo de adesão que especifica as informações extras que serão solicitadas para a participação no programa, bem como política de privacidade e o sigilo com que serão tratadas essas informações.
+				</p>
+				
+
+				<li>Sobre o acesso às suas informações pessoais</li>
+				<br>
+
+				<p>
+					Poderão ver suas informações pessoais apenas funcionários da Eficaz System. Eventualmente, caso a inserção de suas informações se dê em ações criadas em parcerias, os parceiros explicitamente identificados também terão acesso à informação. Nenhuma informação pessoal poderá ser divulgada publicamente.
+				</p>
+
+				<p>
+					A Eficaz System também se compromete a não vender, alugar ou repassar suas informações para terceiros. A única exceção está em casos em que essas informações forem exigidas judicialmente.
+				</p>
+
+				<p>
+					Além disso, embora trabalhemos com boas práticas de proteção e segurança, nenhum serviço web possui 100% de garantia contra invasões e não podemos nos responsabilizar caso isso ocorra.
+				</p>
+
+				<li>Sobre o compartilhamento de conteúdo nas redes sociais</li>
+				<br>
+
+				<p>
+					Ao clicar nos botões de compartilhamento de conteúdo nas redes sociais disponíveis em nossas páginas, o usuário estará publicando o conteúdo por meio de seu perfil na rede selecionada. A Eficaz System não têm acesso ao login e senha dos usuários nessas redes, nem publicará conteúdo em nome do usuário sem que ele realize esta ação.
+				</p>
+
+				<li>Sobre o uso de informações fornecidas a nossos parceiros</li>
+				<br>
+
+				<p>
+					A Eficaz System disponibiliza a seus parceiros, por meio do site, ferramentas para criação de Landing Pages, CRM, envio de email marketing, entre outras que envolvem o gerenciamento de informações pessoais de usuários com quem seus clientes mantêm relacionamento.
+				</p>
+				<p>
+					No entanto, a Eficaz System não controla as informações solicitadas utilizando seu sistema. Esses dados pertencem aos parceiros, que os utilizam, divulgam e protegem de acordo com suas políticas de privacidade. São eles também os responsáveis por coletar, gerenciar e processar as informações confidenciais.
+				</p>
+
+				<li>Sobre o cancelamento da assinatura e alteração/exclusão de informações pessoais</li>
+				<br>
+
+				<p>
+					Você pode optar por não receber mais qualquer tipo de email da Eficaz System.
+				</p>
+				<p>
+					Em todos os emails que enviamos há sempre um link para cancelar a assinatura disponível nas últimas linhas. Ao clicar nesse link você será automaticamente descadastrado da lista.
+				</p>
+
+				<p>
+					É importante mencionar que ao preencher qualquer formulário novamente ficará caracterizada a reinserção do seu email à lista. Portanto, a requisição de cancelamento deve ser feita novamente caso seja de seu interesse.
+				</p>
+
+				<p>
+					Para alterar suas informações pessoais ou mesmo excluí-las do nosso banco de dados, basta enviar um email para sistemaeficaz@sistema.eficazsystem.com.br.
+				</p>
+		
+				<li>Mudanças na Política de Privacidade</li>
+				<br>
+
+				<p>
+					Essa Política de Privacidade pode passar por atualizações. Desta forma, recomendamos visitar periodicamente esta página para que você tenha conhecimento sobre as modificações.
+				</p>
+				<p>
+					Antes de usar informações para outros fins que não os definidos nesta Política de Privacidade, solicitaremos sua autorização.
+				</p>
+
+				<li>Contato com a Eficaz System para esclarecimento de dúvidas</li>
+				<br>
+
+				<p>
+					Qualquer dúvida em relação à nossa política de privacidade pode ser esclarecida entrando em contato conosco.
+				</p>
+
+				<p>
+					Envie um email para <a href="mailto:contato@eficazsystem.com.br?Subject=Entre%20em%20contato" target="_top">contato@sistema.eficazsystem.com.br</a>.
+				</p>
+				<p class="text-left">
+					Estes termos e condições gerais de uso aplicam-se aos serviços prestados pela pessoa jurídica, devidamente registrada sob o CNPJ n. 08.488.358/0001-17 , com sede em:
+				</p>
+				<p class="text-left">
+					Rua Norberto de Brito, 1131
+				</p>
+				<p class="text-left ">
+					São José dos Pinhais,
+				</p>
+				<p class="text-left ">
+					Paraná
+				</p>
+
+			</ol>
+
+		</div>
+
+
+		<div class="container">
+
+			<p class="text-center text-muted">
+				Seja bem-vindo(a)!
+			</p>
+
+			<p class="text-center text-muted">
+
+				A equipe do site Parcerias Eficaz System lhe deseja uma excelente navegação!
+
+			</p>
+
+		</div>
+
+	</section>
 
 @endsection()
--- app/views/sitePrincipal/termos.blade.php
+++ app/views/sitePrincipal/termos.blade.php
@@ -85,21 +85,142 @@
 					<p>
 						Os serviços disponibilizados no site apenas poderão ser acessados por pessoas plenamente capazes, conforme o Direito brasileiro. Todos aqueles que não possuírem plena capacidade civil - menores de 18 anos não emancipados, pródigos, ébrios habituais ou viciados em tóxicos e pessoas que não puderem exprimir sua vontade, por motivo transitório ou permanente - deverão ser devidamente assistidos por seus representantes legais, que se responsabilizarão pelo cumprimento das presentes regras.
 					</p>
-					
+					<p>
+						Ao usuário, será permitido manter apenas uma conta junto ao site. Contas duplicadas serão automaticamente desativadas pelo editor do site, sem prejuízo de demais penalidades cabíveis.
+					</p>
+
+					<p>
+						Para o devido cadastramento junto ao serviço, o usuário deverá fornecer integralmente os dados requeridos. Todas as informações fornecidas pelo usuário devem ser precisas, verdadeiras e atualizadas. Em qualquer caso, o usuário responderá, em âmbito cível e criminal, pela veracidade, exatidão e autenticidade dos dados informados.
+					</p>
+					<p>
+						O usuário deverá fornecer um endereço de e-mail válido, através do qual o site realizará todos os contatos necessários. Todas as comunicações enviadas para o referido endereço serão consideradas lidas pelo usuário, que se compromete, assim, a consultar regularmente suas mensagens recebidas e a respondê-las em prazo razoável.
+					</p>
+
 					<li>DAS RESPONSABILIDADES</li>
 					<br>
+					<p>
+						O editor se responsabilizará pelos defeitos ou vícios encontrados nos serviços prestados pelo site <b>Parcerias Eficaz System</b>, desde que tenha lhes dado causa. Defeitos ou vícios técnicos ou operacionais originados no próprio sistema do usuário não serão de responsabilidade do editor.
+					</p>
+
+					<p>
+						O editor responsabiliza-se apenas pelas informações que foram por ele diretamente divulgadas. Quaisquer informações incluídas pelos usuários, tais como em comentários e em perfis pessoais, serão de inteira responsabilidade dos próprios.
+					</p>
+
+					<p>
+						O usuário é responsável, ainda:
+					</p>
+					<ol style="list-style:lower-alpha">
+						<li> pela correta utilização do site e de seus serviços, prezando pela boa convivência, pelo respeito e pela cordialidade no relacionamento com os demais usuários.</li>
+						<li> pelo cumprimento das regras contidas neste instrumento, bem como normas de Direito nacional e de Direito internacional.</li>
+						<li> pela proteção dos dados de acesso à sua conta (login e senha).</li>
+					</ol>
+
+					<br>
+					<p>
+						O editor não será responsável:
+					</p>
+					<ol style="list-style:lower-alpha">
+						<li> pelas características intrínsecas da internet, principalmente relativas à confiabilidade e à procedência das informações circulando nesta rede.</li>
+						<li> pelos conteúdos ou atividades ilícitas praticadas através de seu site.</li>
+					</ol>
+					<br>
+
 					<li>DOS LINKS EXTERNOS</li>
 					<br>
+					<p>
+						O site <b>Parcerias Eficaz System</b> pode conter links externos redirigindo o usuário para outras páginas da internet, sobre os quais o editor não exerce controle. Apesar das verificações prévias e regulares realizadas pelo editor, ele se isenta de qualquer responsabilidade sobre o conteúdo encontrado nestes sites e serviços.
+					</p>
+					<p>
+						Poderão ser incluídos links nas páginas e nos documentos do site <p>Parcerias Eficaz System</p>, desde que não sirvam para fins comerciais ou publicitários. Esta inclusão dependerá de autorização prévia do editor.
+					</p>
+					<p>
+						Não será autorizada a inclusão de páginas que divulguem quaisquer tipos de informações ilícitas, violentas, polêmicas, pornográficas, xenofóbicas, discriminatórias ou ofensivas.
+					</p>
 					<li>DOS DIREITOS AUTORAIS</li>
 					<br>
+					<p>
+						A estrutura do site, bem como os textos, os gráficos, as imagens, as fotografias, os sons, os vídeos e as demais aplicações informáticas que o compõem são de propriedade do editor e são protegidas pela legislação brasileira e internacional referente à propriedade intelectual.
+					</p>
+					<p>
+						Qualquer representação, reprodução, adaptação ou exploração parcial ou total dos conteúdos, marcas e serviços propostos pelo site, por qualquer meio que seja, sem autorização prévia, expressa e escrita do editor, é estritamente vedada, podendo-se recorrer às medidas cíveis e penais cabíveis. Estão excluídos desta previsão apenas os elementos que estejam expressamente designados no site como livres de direitos autorais.
+					</p>
+					<p>
+						O acesso não gera para o usuário qualquer direito de propriedade intelectual relativo a elementos do site, os quais restam sob a propriedade exclusiva do editor.
+					</p>
+					<p>
+						É vedado ao usuário incluir no site dados que possam modificar o seu conteúdo ou sua aparência.
+					</p>
 					<li>DA POLÍTICA DE PRIVACIDADE</li>
 					<br>
+					<ol style="list-style:lower-alpha">
+						<li> Dados pessoais</li>
+						<p>
+							Todos os dados pessoais fornecidos pelo usuário no momento de seu cadastro ou que venham a ser solicitados posteriormente, a este título, permanecerão sigilosos e não serão repassados a quaisquer parceiros do site.
+						</p>
+						<p>
+							O editor apenas poderá transmitir os dados pessoais a terceiros quando o usuário permiti-lo expressamente ou quando presentes as hipóteses legais previstas no Direito brasileiro, tais como a requisição judicial.
+						</p>
+						<p>
+							É de responsabilidade do editor a garantia de confidencialidade dos dados pessoais fornecidos pelos usuários, devendo proteger o site contra tentativas de violações ou acessos clandestinos à sua base de dados.
+						</p>
+						<li> Dados de navegação</li>
+						<p>
+							O site recorre eventualmente às técnicas de "cookies", que lhe permitem analisar as estatísticas e as informações sobre a navegação do usuário. Podem ser fornecidos, por exemplo, dados sobre o dispositivo utilizado pelo usuário e o seu local de acesso. Esta coleta de informações busca melhorar a navegação, para o conforto do usuário, ao permitir apresentar-lhe serviços personalizados, de acordo com suas preferências.
+						</p>
+						<p>
+							Estes dados de navegação poderão, ainda, ser compartilhados com eventuais parceiros do site, buscando o aprimoramento dos produtos e serviços ofertados ao usuário.
+						</p>
+						<p>
+							O usuário poderá se opor ao registro de "cookies" pelo site, bastando desativar esta opção no seu próprio navegador ou aparelho. Por outro lado, a desativação deste registro poderá afetar a disponibilidade de algumas ferramentas e alguns serviços do site.
+						</p>
+					</ol>
 					<li>DO SERVIÇO DE ATENDIMENTO AO USUÁRIO</li>
 					<br>
+
+					<p>
+						Em caso de dúvidas, sugestões ou problemas com a utilização do site <b>Parcerias Eficaz System</b>, o usuário poderá contatar diretamente o seu serviço de atendimento, através do endereço de e-mail: <a href="mailto:contato@eficazsystem.com.br?Subject=Entre%20em%20contato" target="_top">contato@sistema.eficazsystem.com.br</a>, bem como do telefone: <a  href="tel:+554135563211">(41) 3556-3211</a> / <a href="tel:+554130581736">(41) 3058-1736</a>. Estes serviços de atendimento ao usuário estarão disponíveis nos seguintes dias e horários:
+					</p>
+					<p>
+						Seg à Sex Das 08:00 às 18:00
+					</p>
+
+					<p>
+						O usuário poderá, ainda, optar por enviar correspondência ao endereço da sede do site <b>Parcerias Eficaz System</b>, informado no início deste documento.
+					</p>
+
 					<li>DAS SANÇÕES</li>
 					<br>
+					<p>
+						Sem prejuízo das demais medidas legais cabíveis, o editor do site <b>Parcerias Eficaz System</b> poderá, a qualquer momento, advertir, suspender ou cancelar a conta do usuário:
+					</p>
+					
+					<ol style="list-style:lower-alpha">
+						<li> Que descumprir quaisquer dos dispositivos contidos no presente instrumento.</li>
+						<li> Que descumprir os seus deveres de usuário.</li>
+						<li> Que praticar atos fraudulentos ou dolosos.</li>
+						<li> Cujo comportamento constitua ou possa vir a importar ofensa ou danos a terceiros ou ao próprio site.</li>
+					</ol>
+					<br>
+
 					<li>DAS ALTERAÇÕES</li>
 					<br>
+
+					<p>
+						A presente versão dos termos e condições gerais de uso foi atualizada pela última vez em: 16/11/2017
+					</p>
+					<p>
+						O editor se reserva o direito de modificar, a qualquer momento e sem qualquer aviso prévio, o site e os serviços, bem como as presentes normas, especialmente para adaptá-las às evoluções do site <b>Parcerias Eficaz System</b>, seja pela disponibilização de novas funcionalidades, seja pela supressão ou modificação daquelas já existentes.
+					</p>
+					
+					<li>DAS ALTERAÇÕES</li>
+					<br>
+
+					<p>
+						Para a solução das controvérsias decorrentes do presente instrumento, será aplicado integralmente o Direito brasileiro.
+					</p>
+					<p>
+						Os eventuais litígios deverão ser apresentados no foro da comarca em que se encontra a sede do editor do site.
+					</p>
 				</ol>
 
 			</div>
